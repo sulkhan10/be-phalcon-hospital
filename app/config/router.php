139cd6c33f8a810518239640d2aae6078c971bdb
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-09-17 01:06:51 +0700

    feat : init router

--- app/config/router.php
+++ app/config/router.php
@@ -1,7 +1,12 @@
 <?php
-
 $router = $di->getRouter();
 
 // Define your routes here
+$router->add('/patients', 'Index::index')->via(['GET']);
+$router->add('/patients', 'Index::create')->via(['POST']);
+$router->add('/patients/{id:[0-9]+}', 'Index::update')->via(['PUT']);
+$router->add('/patients/{id:[0-9]+}', 'Index::show')->via(['GET']);
+$router->add('/patients/{id:[0-9]+}', 'Index::delete')->via(['DELETE']);
 
 $router->handle($_SERVER['REQUEST_URI']);
+
